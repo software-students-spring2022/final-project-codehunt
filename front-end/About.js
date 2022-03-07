ffc129f4286319954a054c9cacfc1d919d20ca7f
Andrew Shin <andrewshin02@gmail.com>
2022-03-07 13:30:38 -0500

    adding about us things

---
+++ front-end/About.js
@@ -0,0 +1,29 @@
+import {BrowserRouter as Router, Routes, Route } from "react-browser-dom"
+import "./About.css"
+
+
+const About = props => {
+    return (
+        <main className = "Home">
+            <h1>About CodeHunt</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            <p>Eu nisl nunc mi ipsum faucibus vitae aliquet nec. Accumsan lacus vel facilisis volutpat est velit egestas.</p>
+            <p>Consectetur lorem donec massa sapien faucibus et molestie. Donec adipiscing tristique risus nec feugiat in fermentum posuere urna.</p>
+            <p>Scelerisque varius morbi enim nunc faucibus a pellentesque sit amet. Arcu risus quis varius quam quisque.</p>
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+            <p>© 2022 CodeHunt</p>
+        </main>
+    )
+}
+
+
+export default About
