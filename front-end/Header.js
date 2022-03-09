9a8d5c0049d57d59741cf6d01222bd5a5b91a193
Charlie Cai <lc4181@nyu.edu>
2022-03-08 23:36:53 -0500

    update

---
+++ front-end/Header.js
@@ -0,0 +1,12 @@
+import { Link } from 'react-router-dom'
+import './Header.css'
+
+const Header = props => {
+    return (
+        <Header className = "Header-header">
+            <nav className="Header-nav">
+                
+            </nav>
+        </Header>
+    )
+}
