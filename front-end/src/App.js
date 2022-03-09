882e6218c8997c36792bab1ace044894735447aa
Charlie Cai <lc4181@nyu.edu>
2022-03-09 01:30:23 -0500

    moved old file to correct folder

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router, Routes, Route } from "react-browser-dom"
+import './App.css'
+import Home from "./Home"
+// import List from "./List"
+// import Login from "./Login"
+// import SignUp from "./SignUp"
+// import ForgetPassword from "./ForgetPassword"
+// import UserSettings from "./UserSettings"
+import About from "./About"
+import Header from "./Header"
+import Contest from "./Contest"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = props => {
+    return (
+        <div className="App">
+            <Router>
+                <Header />
+                <main className="App-main">
+                    <Routes>
+                        <Route path="/" element={<Home />}></Route>
+                        {/*<Route path="/List" element={<List />}></Route>*/}
+                        {/*<Route path="/Login" element={<Login />}></Route>*/}
+                        {/*<Route path="/SignUp" element={<SignUp />}></Route>*/}
+                        {/*<Route path="/ForgetPassword" element={<ForgetPassword />}></Route>*/}
+                        {/*<Route path="/UserSettings" element={<UserSettings />}></Route>*/}
+                        <Route path="/About" element={<About />}></Route>
+                    </Routes>
+                </main>
+            </Router>
+        </div>
+    )
+};
 
-export default App;
+export default App
