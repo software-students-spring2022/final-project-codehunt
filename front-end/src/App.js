11eba395a299cb53cbc89c8c4fa8017da837477b
Charlie Cai <lc4181@nyu.edu>
2022-03-18 18:57:15 -0400

    update

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Header from "./Header"
 import Home from "./Home"
-import List from "./List"
+import List from "./ContestList"
+import Settings from "./Settings.js"
 import Login from "./Login"
 import SignUp from "./SignUp"
 import ForgetPassword from "./ForgetPassword"
-import UserSettings from "./UserSettings"
 import About from "./About"
-import Header from "./Header"
-import Settings from "./Settings.js"
 
 const App = props => {
     return (
@@ -17,14 +16,13 @@ const App = props => {
             <BrowserRouter>
                 <Header />
                     <Switch>
-                        <Route path="/" element={<Home />}/>
-                        <Route path="/List" element={<List />}/>
-                        <Route path="/Login" element={<Login />}/>
-                        <Route path="/SignUp" element={<SignUp />}/>
-                        <Route path="/ForgetPassword" element={<ForgetPassword />}/>
-                        <Route path="/UserSettings" element={<UserSettings />}/>
-                        <Route path="/About" element={<About />}/>
-                        <Route path="/Settings" element={<Settings />}/>
+                        <Route path="/" exact component={Home} />
+                        <Route path="/list" exact component={List} />
+                        <Route path="/settings" exact component={Settings} />
+                        <Route path="/login" exact component={Login} />
+                        <Route path="/signup" exact component={SignUp} />
+                        <Route path="/forgetPassword" exact component={ForgetPassword} />
+                        <Route path="/about" exact component={About} />
                     </Switch>
             </BrowserRouter>
         </div>
