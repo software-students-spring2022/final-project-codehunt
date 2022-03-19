1f58c231dc51e6c51a18fa0e80f77d79353ea8ea
Charlie Cai <lc4181@nyu.edu>
2022-03-18 22:16:08 -0400

    Update App.js

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,6 +1,6 @@
 import React from "react"
 import "./App.css"
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 import { Helmet } from "react-helmet"
 import Header from "./Header"
 import Home from "./Home"
@@ -13,29 +13,28 @@ import About from "./About"
 
 const App = props => {
   return (
-    <div className="App">
-      <Helmet>
-        <link
-          rel="stylesheet"
-          href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
-          integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
-          crossOrigin="anonymous"
-        />
-      </Helmet>
-      <BrowserRouter>
-        <Header />
-          <Route>
-            <Route path="/" element={<Home />}/>
-            <Route path="/list" element={<List />}/>
-            <Route path="/login" element={<Login />}/>
-            <Route path="/signup" element={<Signup />}/>
-            <Route path="/forgetPassword" element={<ForgetPassword />}/>
-            <Route path="/userSettings" element={<UserSettings />}/>
-            <Route path="/about" element={<About />}/>
-            <Route path="/settings" element={<Settings />}/>
-          </Route>
-      </BrowserRouter>
-    </div>
+      <div className="App">
+          <Helmet>
+              <link
+                  rel="stylesheet"
+                  href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+                  integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
+                  crossOrigin="anonymous"
+              />
+          </Helmet>
+          <BrowserRouter>
+              <Header />
+              <Routes>
+                  <Route path="/" element={<Home />}/>
+                  <Route path="/list" element={<List />}/>
+                  <Route path="/settings" element={<Settings />}/>
+                  <Route path="/login" element={<Login />}/>
+                  <Route path="/signup" element={<SignUp />}/>
+                  <Route path="/forgetPassword" element={<ForgetPassword />}/>
+                  <Route path="/about" element={<About />}/>
+              </Routes>
+          </BrowserRouter>
+      </div>
   )
 };
 
