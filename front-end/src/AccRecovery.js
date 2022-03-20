dce84833cb018c7bfbb5d8fc8e08a0eab897d1fa
Michael Zhou <michaelzhou7@gmail.com>
2022-03-20 17:58:36 -0400

    added acc recovery

---
+++ front-end/src/AccRecovery.js
@@ -0,0 +1,22 @@
+import FloatingLabel from "react-bootstrap/FloatingLabel";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import "./AccRecovery.css"
+
+export default function Signin() {
+  return (
+    <div class="flex-container flex-center">
+      <Form className="p24 wmin450 bar-lg auth-shadow">
+        <div class="mb24 wmax450">Forgot your account’s password or having trouble logging in? Enter your email address and we’ll send you a recovery link.</div>
+        <Form.Group className="mb-3" controlId="formSignin">
+          <FloatingLabel controlId="floatingInput" label="Email" className="mb-3">
+            <Form.Control type="email" placeholder="Email" />
+          </FloatingLabel>
+        </Form.Group>
+        <Button className="w100" variant="primary" type="submit">
+          Send Recovery Email
+        </Button>
+      </Form>
+    </div>
+  )
+}
