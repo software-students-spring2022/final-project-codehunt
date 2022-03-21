e00545244ba8faabec0b6654e5c07dd22ed174d1
dana-sun <ds5613@nyu.edu>
2022-03-21 11:22:27 -0400

    added css to settings

--- front-end/src/Settings.js
+++ front-end/src/Settings.js
@@ -2,34 +2,38 @@ import React, { useState } from 'react';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import BootstrapSwitchButton from 'bootstrap-switch-button-react'
+import './settings.css'
 
 export default function Signup() {
   return (
-    <div className='Settings'>
-      <h1>User Settings</h1>
-      <Form>
-        <Form.Group controlId='name' size='lg'>
-          <Form.Label>Name</Form.Label>
-          <Form.Control type='name' placeholder="Enter name"/>
+    <div class='settings'>
+      <h1 class ="setting">User Settings</h1>
+      <Form className="info">
+        <Form.Group controlId='name-info' size='lg'>
+          <Form.Label className="name">Name</Form.Label>
+          <br></br>
+          <Form.Control type='name-change' placeholder="Enter name"/>
           <a href="/">Edit</a>
         </Form.Group>
         <Form.Group controlId='email' size='lg'>
           <Form.Label>Email Address</Form.Label>
+          <br></br>
           <Form.Control type="email" placeholder="Enter email"/>
           <a href="/">Edit</a>
         </Form.Group>
         <Form.Group controlId='password' size='lg'>
           <Form.Label>Password</Form.Label>
+          <br></br>
           <Form.Control type="password" placeholder="Enter password"/>
           <a href="/">Reset</a>
         </Form.Group>
-        <Button variant="primary" type="submit">
-          Submit
-        </Button>
       </Form>
       <Form>
-        <h2>Subscriptions</h2>
-        <a href="/">Manage</a>
+        <section class="container">
+          <h2 class="subs">Subscriptions</h2>
+          <a href="/">Manage</a>
+        </section>
+        
         {['radio'].map((type) => (
           <div key={`default-${type}`} className="mb-3">
             <Form.Check 
@@ -47,12 +51,11 @@ export default function Signup() {
           </div>
         ))}
       </Form>
-      <h3>Email Notifications</h3>
-      <BootstrapSwitchButton checked={true} size="sm"/>
-      <a href="/">Sign Out</a>
+      <h3 class="notifs">Email Notifications</h3>
+      <div class="bootbutton">
+        <BootstrapSwitchButton checked={true} size="sm"/>
+      </div>
+      <div class="sign-out"><a href="/">Sign Out</a></div>
     </div>
-
-    
-   
   );
 }
