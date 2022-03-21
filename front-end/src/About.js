c55b00f0162616a65368052b71b1ff12a455a225
Andrew Shin <andrewshin02@gmail.com>
2022-03-21 13:16:31 -0400

    improved about us

--- front-end/src/About.js
+++ front-end/src/About.js
@@ -6,6 +6,16 @@ const About = props => {
         <main className = "Home">
             <h1>About CodeHunt</h1>
             <p>CodeHunt is a platform that assembles and organizes various coding contests’ information and details. Compatible with popular websites (including LeetCode, CodeForces, and Kaggle), CodeHunt eliminates the need to visit each individually to stay updated. Keeping track of competitions has never been easier.</p>
+            <p>Our goal? By simplifying how students search for coding contests, we want them to easily and efficiently approach their goal of going to their dream company and landing their dream job. All they would need to do is subscribe to the websites whose contests they are interested in participating in. That’s it! No more inconvenient jumping from site to site. No more hectic scheduling. Keeping track of competitions has never been easier.</p>
+            <p>This platform is dedicated to CS students at NYU so far due to limited resources. We hope to expand this demographic through further developments and improvements.</p>
+            <p>CodeHunt was created by:
+                <a href="https://github.com/charliecai00">Charlie Cai</a>
+                <a href="https://github.com/dinanz">Diana Zhao</a>
+                <a href="https://github.com/b0ub0">Chengyang Song</a>
+                <a href="https://github.com/kopokopok">Michael Zhou</a>
+                <a href="https://github.com/dana-sun">Dana Sun</a>
+                <a href="https://github.com/andrewshin02">Andrew Shin</a>
+            </p>
             <p>© 2022 CodeHunt</p>
         </main>
     )
