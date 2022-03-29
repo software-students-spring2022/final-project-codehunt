d091280968dc4a1a4de130a5315c83a12fb76fc9
Michael Zhou <michaelzhou7@gmail.com>
2022-03-29 00:16:34 -0400

    changed eslint rules, syntax formatting

--- front-end/src/Login.js
+++ front-end/src/Login.js
@@ -1,7 +1,7 @@
-import FloatingLabel from 'react-bootstrap/FloatingLabel';
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
-import './Login.css';
+import FloatingLabel from "react-bootstrap/FloatingLabel"
+import Form from "react-bootstrap/Form"
+import Button from "react-bootstrap/Button"
+import "./Login.css"
 
 export default function Login() {
   return (
@@ -9,13 +9,22 @@ export default function Login() {
       <Form className="p24 wmin450 bar-lg auth-shadow">
         <h3 className="ta-center mb24 c-white">Welcome Back!</h3>
         <Form.Group className="mb-3" controlId="formSignin">
-          <FloatingLabel controlId="floatingInput" label="Email" className="mb-3">
+          <FloatingLabel
+            controlId="floatingInput"
+            label="Email"
+            className="mb-3"
+          >
             <Form.Control type="email" placeholder="Email" />
           </FloatingLabel>
           <FloatingLabel controlId="floatingPassword" label="Password">
             <Form.Control type="password" placeholder="Password" />
           </FloatingLabel>
-          <a className="fs-body1" href="/account-recovery">Forgot your password?</a>
+          <a
+            className="fs-body1"
+            href="/account-recovery"
+          >
+            Forgot your password?
+          </a>
         </Form.Group>
         <Button className="w100" variant="primary" type="submit">
           Sign In
@@ -23,5 +32,5 @@ export default function Login() {
         <a className="fs-body1" href="/signup">Need an account?</a>
       </Form>
     </div>
-  );
+  )
 }
