69ef8f5b90ffc666a1ea581998b6b9a0356d92eb
Michael Zhou <michaelzhou7@gmail.com>
2022-03-29 00:16:34 -0400

    changed eslint rules, syntax formatting

--- front-end/src/AccRecovery.js
+++ front-end/src/AccRecovery.js
@@ -1,15 +1,22 @@
-import FloatingLabel from 'react-bootstrap/FloatingLabel';
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
-import './AccRecovery.css';
+import FloatingLabel from "react-bootstrap/FloatingLabel"
+import Form from "react-bootstrap/Form"
+import Button from "react-bootstrap/Button"
+import "./AccRecovery.css"
 
 export default function Signin() {
   return (
     <div className="flex-container flex-center">
       <Form className="p24 wmin450 bar-lg auth-shadow">
-        <div className="mb24 wmax450 c-white">Forgot your account’s password or having trouble logging in? Enter your email address and we’ll send you a recovery link.</div>
+        <div className="mb24 wmax450 c-white">
+          Forgot your account’s password or having trouble logging in?
+          Enter your email address and we’ll send you a recovery link.
+        </div>
         <Form.Group className="mb-3" controlId="formSignin">
-          <FloatingLabel controlId="floatingInput" label="Email" className="mb-3">
+          <FloatingLabel
+            controlId="floatingInput"
+            label="Email"
+            className="mb-3"
+          >
             <Form.Control type="email" placeholder="Email" />
           </FloatingLabel>
         </Form.Group>
@@ -18,5 +25,5 @@ export default function Signin() {
         </Button>
       </Form>
     </div>
-  );
+  )
 }
