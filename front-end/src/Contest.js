0a38ab5a31fd45cf4c7fc86c72b140bb74d34236
dinanz <dz1371@nyu.edu>
2022-03-30 13:45:36 -0400

    fix contest len

--- front-end/src/Contest.js
+++ front-end/src/Contest.js
@@ -11,20 +11,23 @@ const Contest = (props) => {
   // contest description
   return (
     <div>
-      <meta className="viewport" content="width=device-width, initial-scale=1"></meta>
+      <meta className="viewport" content="width=device-width, initial-scale=1">
+      </meta>
       <div className="contestlist-cards"></div>
       <div className={"contestlist-card contestlist-card-0"}>
         <div className="contestlist-card__icon">
-          <img alt="logo" src="./../images/{props.platform}.png" ></img>
+          <img alt="logo" src="./../images/${props.platform}.png" ></img>
         </div>
-        <p className="contestlist-card__exit"><i className="fas fa-times"></i></p>
+        <p className="contestlist-card__exit"><i className="fas fa-times">
+        </i></p>
         <h2 className="contestlist-card__title">{props.name}</h2>
         <div className="contestlist-card__content">
           <p>Time: {props.start_date} - {props.end_date}</p>
           <p>Details: {props.description}</p>
         </div>
         <p className="contestlist-card__apply">
-          <a className="contestlist-card__link" target="_blank" rel="noopener noreferrer" href={props.link}>
+          <a className="contestlist-card__link" target="_blank"
+            rel="noopener noreferrer" href={props.link}>
             Go to Website <i className="fas fa-arrow-right"></i></a>
         </p>
       </div>
