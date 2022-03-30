83ed0bb95384aeea2c0f939bfbd107d89930cde6
Michael Zhou <michaelzhou7@gmail.com>
2022-03-30 16:28:04 -0400

    more backend setup

--- back-end/app.js
+++ back-end/app.js
@@ -1,5 +1,12 @@
-const express = require('express');
-const app = express();
+const express = require("express")
+const morgan = require("morgan")
+const axios = require("axios")
+require("dotenv").config({silent: true})
 
+const app = express()
 
-module.exports = app;
+app.use(morgan("dev"))
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+module.exports = app
