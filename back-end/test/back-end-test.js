4ab7c3d64f8baa5b2050b103d5d7f3e4ebbb0e7f
Charlie Cai <lc4181@nyu.edu>
2022-04-04 18:17:03 -0400

    set up unit testing

---
+++ back-end/test/back-end-test.js
@@ -0,0 +1,9 @@
+const mocha = require('mocha');
+const chai = require('chai');
+const expect = chai.expect;
+const chai_http = require('chai-http');
+const net = require('net');
+const path = require('path');
+require('sinon');
+
+
