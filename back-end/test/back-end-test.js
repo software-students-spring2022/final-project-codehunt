c9be565875622065b5b44c00c7d78d6fb2416320
Charlie Cai <lc4181@nyu.edu>
2022-04-08 01:48:55 -0400

    moved file to folder

--- back-end/test/back-end-test.js
+++ back-end/test/back-end-test.js
@@ -7,7 +7,7 @@ const path = require('path');
 require('sinon');
 
 const assert = require('assert');
-const server = require('../app')
+const server = require('../src/app')
 
 describe("server", function () {
     it("listen on port 3000", function () {
