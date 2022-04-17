bd4039dda6a96666b23eb66e1c37ce37a7c53871
Michael Zhou <michaelzhou7@gmail.com>
2022-04-17 15:29:22 -0400

    minor changes

--- back-end/src/db.js
+++ back-end/src/db.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose")
 const User = new mongoose.Schema({
   email: {type: String, required: true},
   password: {type: String, required: true},
+  subscriptions: {type: Object},
 })
 
 mongoose.model("User", User)
