9160693a88a503fbe0503c4c6c71d6d467acf916
Michael Zhou <michaelzhou7@gmail.com>
2022-04-16 22:01:46 -0400

    implementing database authentication

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -1,8 +1,8 @@
 require("dotenv").config({silent: true})
 const path = require("path")
 const fs = require("fs")
-const mongoose = require("mongoose")
 require("./db")
+const {jwtOptions, jwtStrategy} = require("./jwt-config.js")
 
 // import and instantiate express
 const express = require("express")
@@ -17,10 +17,6 @@ const morgan = require("morgan")
 const jwt = require("jsonwebtoken")
 const passport = require("passport")
 const cors = require("cors")
-
-const users = require("../model/user.json") // mock user data
-const _ = require("lodash") // the lodash module has some convenience functions for arrays that we use to sift through our mock user data... you don't need this if using a real database with user info
-const {jwtOptions, jwtStrategy} = require("./jwt-config.js")
 
 app.use(morgan("dev"))
 app.use(express.json())
@@ -43,7 +39,7 @@ app.get(
         success: true,
         user: {
           id: req.user.id,
-          username: req.user.username,
+          username: req.user.email,
         },
         message: "Congratulations: you have accessed this route because you have a valid JWT token!",
       })
@@ -51,32 +47,33 @@ app.get(
 )
 
 app.post("/login", (req, res) => {
-  const username = req.body.username
+  const email = req.body.username
   const password = req.body.password
 
-  if (!username || !password) {
+  if (!email || !password) {
     res
         .status(401)
-        .json({success: false, message: "no username or password supplied."})
+        .json({success: false, message: "no email or password supplied."})
   }
 
-  const user = users[_.findIndex(users, {username: username})]
-  if (!user) {
-    res
-        .status(401)
-        .json({success: false, message: `user not found: ${username}.`})
-  } else if (req.body.password === user.password) {
-    // assuming we found the user, check the password is correct
-    // we would normally encrypt the password the user submitted to check it against an encrypted copy of the user's password we keep in the database... but here we just compare two plain text versions for simplicity
-    // the password the user entered matches the password in our "database" (mock data in this case)
-    // from now on we'll identify the user by the id and the id is the only personalized value that goes into our token
-    const payload = {id: user.id} // some data we'll encode into the token
-    const token = jwt.sign(payload, jwtOptions.secretOrKey) // create a signed token
-    res.status(200).json({success: true, username: user.username, token: token}) // send the token to the client to store
-  } else {
-    // the password did not match
-    res.status(401).json({success: false, message: "passwords did not match"})
-  }
+  User.findOne({email: email}, (err, user) => {
+    if (!user) {
+      res
+          .status(401)
+          .json({success: false, message: `user not found: ${email}.`})
+    } else if (req.body.password === user.password) {
+      // assuming we found the user, check the password is correct
+      // we would normally encrypt the password the user submitted to check it against an encrypted copy of the user's password we keep in the database... but here we just compare two plain text versions for simplicity
+      // the password the user entered matches the password in our "database" (mock data in this case)
+      // from now on we'll identify the user by the id and the id is the only personalized value that goes into our token
+      const payload = {id: user.id} // some data we'll encode into the token
+      const token = jwt.sign(payload, jwtOptions.secretOrKey) // create a signed token
+      res.status(200).json({success: true, username: user.email, token: token}) // send the token to the client to store
+    } else {
+      // the password did not match
+      res.status(401).json({success: false, message: "passwords did not match"})
+    }
+  })
 })
 
 app.get("/get/contests", (req, res) => {
