fe7c33c71404189c3c15ab74023eba48877c8062
Michael Zhou <michaelzhou7@gmail.com>
2022-04-22 10:19:04 -0400

    syntax fix

--- front-end/src/js/Home.js
+++ front-end/src/js/Home.js
@@ -28,7 +28,7 @@ const Home = () => {
 
       <section className = "ContestsBlock">
 
-         {featuredContests.map((contest) => (
+        {featuredContests.map((contest) => (
           <Contest key={contest.id}
             name = {contest.name}
             platform={contest.platform}
@@ -38,7 +38,7 @@ const Home = () => {
             link={contest.url}
             logo={contest.logo}
           />
-        ))} 
+        ))}
 
       </section>
     </div>
